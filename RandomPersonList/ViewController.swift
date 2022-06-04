d5c0c22b5affef797a1d5dae2c4940c7dd68352f
aiv <96691834+aiv26@users.noreply.github.com>
2022-06-04 17:13:46 +0300

    Initial Commit

---
+++ RandomPersonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPersonList
+//
+//  Created by aiv on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
