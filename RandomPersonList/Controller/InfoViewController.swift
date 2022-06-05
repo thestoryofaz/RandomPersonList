ca334d67b246e7bff311aa18528f577e85dcec3a
aiv <96691834+aiv26@users.noreply.github.com>
2022-06-05 21:52:27 +0300

    Finish

--- RandomPersonList/Controller/InfoViewController.swift
+++ RandomPersonList/Controller/InfoViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 class InfoViewController: UIViewController {
 
     @IBOutlet var name: UILabel!
-    @IBOutlet var lastName: UILabel!
     @IBOutlet var email: UILabel!
     @IBOutlet var phoneNumber: UILabel!
     
+    var firstLastName: String!
+    var emails: String!
+    var phone: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        name.text = firstLastName
+        email.text = emails
+        phoneNumber.text = phone
     }
-
-
 }
